2e5634ce00b4e454d41ecd559cf15b69d86481d6
Chirag Goel <96922117+chiggshiggs@users.noreply.github.com>
2023-06-10 02:32:03 +0530

    added stats ui

--- website/app/(landingPage)/page.tsx
+++ website/app/(landingPage)/page.tsx
@@ -1,11 +1,53 @@
 import React from "react"
+import { Card } from "@/components/ui/card"
 
 const page = () => {
   return (
     <>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        some text
-      </section>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="text-center">
+      <div className="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+        <svg className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10" stroke="currentColor" viewBox="0 0 52 52">
+          <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+        </svg>
+      </div>
+      <h6 className="text-4xl font-bold text-deep-purple-accent-400">819</h6>
+      <p className="mb-2 font-bold text-md">Downloads</p>
+     
+    </div>
+    <div className="text-center">
+      <div className="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+        <svg className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10" stroke="currentColor" viewBox="0 0 52 52">
+          <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+        </svg>
+      </div>
+      <h6 className="text-4xl font-bold text-deep-purple-accent-400">1.3K</h6>
+      <p className="mb-2 font-bold text-md">Users</p>
+      
+    </div>
+    <div className="text-center">
+      <div className="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+        <svg className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10" stroke="currentColor" viewBox="0 0 52 52">
+          <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+        </svg>
+      </div>
+      <h6 className="text-4xl font-bold text-deep-purple-accent-400">91</h6>
+      <p className="mb-2 font-bold text-md">Subscribers</p>
+   
+    </div>
+    <div className="text-center">
+      <div className="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+        <svg className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10" stroke="currentColor" viewBox="0 0 52 52">
+          <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+        </svg>
+      </div>
+      <h6 className="text-4xl font-bold text-deep-purple-accent-400">52</h6>
+      <p className="mb-2 font-bold text-md">Products</p>
+      
+    </div>
+  </div>
+</div>
     </>
   )
 }
